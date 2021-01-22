58b9d27633ffd3d5f00c5d6ac9ea8375267b4678
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-01-22 22:25:48 +0100

    feat: added app settings helper

---
+++ src/ImageCropDataTypeGenerator.Umbraco/Helpers/AppSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace ImageCropDataTypeGenerator.Umbraco.Helpers
+{
+    internal static class AppSettings
+    {
+        public static T Get<T>(string key, Action<Exception> errorCallback = null)
+        {
+            var value = default(T);
+
+            var setting = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrWhiteSpace(setting))
+            {
+                return value;
+            }
+
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+
+            try
+            {
+                value = (T)converter.ConvertFromInvariantString(setting);
+            }
+            catch (Exception ex)
+            {
+                errorCallback?.Invoke(ex);
+            }
+
+            return value;
+        }
+    }
+}
