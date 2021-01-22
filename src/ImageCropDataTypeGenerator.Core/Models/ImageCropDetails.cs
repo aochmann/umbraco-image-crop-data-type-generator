b8a9ea46fc16d67d5322006fad7a811fd66d1cb8
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-01-22 13:12:53 +0100

    feat: added model

---
+++ src/ImageCropDataTypeGenerator.Core/Models/ImageCropDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageCropDataTypeGenerator.Core.Models
+{
+    public class ImageCropDetails
+    {
+        public string Alias { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
