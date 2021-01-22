debd3f16131c9f9efe642fa167df5cbf4c6e1393
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-01-22 13:13:39 +0100

    feat: added image crop generator

---
+++ src/ImageCropDataTypeGenerator.Core/ImageCropDataTypeGenerator.cs
@@ -0,0 +1,62 @@
+﻿using ImageCropDataTypeGenerator.Core.Extensions;
+using ImageCropDataTypeGenerator.Core.Interfaces;
+using ImageCropDataTypeGenerator.Core.Models;
+using Scriban;
+using Scriban.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImageCropDataTypeGenerator.Core
+{
+    public class ImageCropDataTypeGenerator : IImageCropDataTypeGenerator
+    {
+        private const string ClassTemplate = @"
+            namespace {{ namespace }}
+            {
+                public static class ImageCropDefinition
+                {
+                    {{~ for imageAliasDefinition in imageAliasDefinitions ~}}
+                    public static class {{ imageAliasDefinition.Name }}
+                    {
+                        public const string Alias = ""{{ imageAliasDefinition.Value }}"";
+                        public const int Width = ""{{ imageAliasDefinition.Width }}"";
+                        public const int Height = ""{{ imageAliasDefinition.Height }}"";
+                    }
+                    {{~ for imageAliasDefinition in imageAliasDefinitions ~}}
+                }
+            }";
+
+        private const string DefaultNamespace = "ImageCropDataTypeGenerator.Models";
+
+        public string Generate(IEnumerable<ImageCropDetails> imageCrops, string @namespace = null)
+        {
+            if (@namespace.HasValue())
+            {
+                @namespace = DefaultNamespace;
+            }
+
+            var imageAliasDefinitions = imageCrops.Select(imageCrop => new
+            {
+                Name = imageCrop.Alias.ToPascalCase(),
+                Value = imageCrop.Alias,
+                imageCrop.Width,
+                imageCrop.Height
+            }).ToArray();
+
+            var context = new TemplateContext { MemberRenamer = member => member.Name };
+
+            var scriptObject = new ScriptObject
+            {
+                ["imageAliasDefinitions"] = imageAliasDefinitions,
+                ["namespace"] = @namespace
+            };
+
+            context.PushGlobal(scriptObject);
+
+            var template = Template.Parse(ClassTemplate);
+            var result = template.Render(context);
+
+            return result;
+        }
+    }
+}
---
+++ src/ImageCropDataTypeGenerator.Core/Interfaces/IImageCropDataTypeGenerator.cs
@@ -0,0 +1,10 @@
+﻿using ImageCropDataTypeGenerator.Core.Models;
+using System.Collections.Generic;
+
+namespace ImageCropDataTypeGenerator.Core.Interfaces
+{
+    public interface IImageCropDataTypeGenerator
+    {
+        string Generate(IEnumerable<ImageCropDetails> imageCrops, string @namespace = null);
+    }
+}
