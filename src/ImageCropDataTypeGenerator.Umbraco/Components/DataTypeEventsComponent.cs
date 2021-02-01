634649cace04a0b2e0a3b3efdf57f36eb2bf3605
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-01 08:21:42 +0100

    feat: added saving generated output to file

--- src/ImageCropDataTypeGenerator.Umbraco/Components/DataTypeEventsComponent.cs
+++ src/ImageCropDataTypeGenerator.Umbraco/Components/DataTypeEventsComponent.cs
@@ -1,6 +1,8 @@
 ﻿using ImageCropDataTypeGenerator.Core.Interfaces;
 using ImageCropDataTypeGenerator.Core.Models;
-using ImageCropDataTypeGenerator.Umbraco.Configurations;
+using ImageCropDataTypeGenerator.Umbraco.Extensions;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Umbraco.Core;
 using Umbraco.Core.Composing;
@@ -9,43 +11,62 @@ using Umbraco.Core.Models;
 using Umbraco.Core.PropertyEditors;
 using Umbraco.Core.Services;
 using Umbraco.Core.Services.Implement;
+using File = System.IO.File;
 
 namespace ImageCropDataTypeGenerator.Umbraco.Components
 {
     internal class DataTypeEventsComponent : IComponent
     {
         private readonly IImageCropDataTypeGenerator _imageCropDataTypeGenerator;
+        private readonly Options.Options _options;
         private readonly IDataTypeService _dataTypeService;
 
         public DataTypeEventsComponent(
             IImageCropDataTypeGenerator imageCropDataTypeGenerator,
+            Options.Options options,
             IDataTypeService dataTypeService)
         {
             _imageCropDataTypeGenerator = imageCropDataTypeGenerator;
+            _options = options;
             _dataTypeService = dataTypeService;
         }
 
         public void Initialize()
         {
+            if (!_options.Enable)
+            {
+                return;
+            }
+
             DataTypeService.Saved += On_Saved;
             DataTypeService.Deleted += On_Deleted;
         }
 
         public void Terminate()
         {
+            if (!_options.Enable)
+            {
+                return;
+            }
+
             DataTypeService.Saved -= On_Saved;
             DataTypeService.Deleted -= On_Deleted;
         }
 
         private void On_Deleted(IDataTypeService sender, DeleteEventArgs<IDataType> e)
-        {
-        }
+            => RecreateImageCropDefinition();
 
         private void On_Saved(IDataTypeService sender, SaveEventArgs<IDataType> e)
+            => RecreateImageCropDefinition();
+
+        private void RecreateImageCropDefinition()
         {
-            var imageCroppers = e.SavedEntities
-                .Where(x => x.EditorAlias == Constants.PropertyEditors.Aliases.ImageCropper)
-                .ToArray();
+            var imageCroppers = GetAllImageCropperDataTypes();
+
+            if (!imageCroppers.HasAny())
+            {
+                return;
+            }
 
             var cropDetails = imageCroppers
                 .SelectMany(imageCropper => (imageCropper.Configuration as ImageCropperConfiguration)
@@ -58,7 +79,29 @@ namespace ImageCropDataTypeGenerator.Umbraco.Components
                         Height = crop.Height
                     }).ToArray());
 
-            var output = _imageCropDataTypeGenerator.Generate(cropDetails, ApplicationConfiguration.GeneratedClassNamespace);
+            var classDefinitionCode = _imageCropDataTypeGenerator.Generate(cropDetails, _options.ModelsNamespace);
+
+            SaveFileClassOutput(classDefinitionCode, _options.ModelsDirectory);
+        }
+
+        private IEnumerable<IDataType> GetAllImageCropperDataTypes()
+            => _dataTypeService.GetByEditorAlias(Constants.PropertyEditors.Aliases.ImageCropper);
+
+        public void SaveFileClassOutput(string content, string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                _ = Directory.CreateDirectory(directory);
+            }
+
+            var outputPath = Path.Combine(directory, "ImageCropDefinition.generated.cs");
+
+            if (File.Exists(outputPath))
+            {
+                File.Delete(outputPath);
+            }
+
+            File.WriteAllText(outputPath, content);
         }
     }
 }
